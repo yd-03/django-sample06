37132c8dfcd8d05ecd9f92b431d9eb1a59884a2b
yd-03 <3nu4ydye57@gmail.com>
2024-10-13 15:11:33 +0900

    feat: ワーカーリストにワーカーのデータを表示するためのテンプレートとビューを追加

--- manager/views.py
+++ manager/views.py
@@ -7,8 +7,13 @@ from manager.models import *
 class WorkerListView(TemplateView):
     template_name = "worker_list.html"
 
+    # HTTP GET リクエストを処理するメソッドを定義
     def get(self, request, *args, **kwargs):
+        # 親クラスの get_context_data メソッドを呼び出して基本的なコンテキストデータを取得
         context = super(WorkerListView, self).get_context_data(**kwargs)
+        # Worker モデルのすべてのインスタンスを取得
         workers = Worker.objects.all()
+        # 取得した workers をコンテキストに追加
         context["workers"] = workers
+        # テンプレートをレンダリングし、HTTP レスポンスを返す
         return render(request, self.template_name, context)
