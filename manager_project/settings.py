93c66a066db78fe521325b1d83ce9e09aedb1ae8
yd-03 <3nu4ydye57@gmail.com>
2024-10-13 23:32:21 +0900

    feat: ワーカーリストのテンプレートとビューを追加

--- manager_project/settings.py
+++ manager_project/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
     "manager",
 ]
 
@@ -110,3 +112,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Static file settings
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
